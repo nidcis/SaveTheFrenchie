a1cf0e5140b5744e6242b0dd9132cd016548041e
David Sajous <david@nidcis.com>
2016-12-20 18:01:44 -0500

    Updated Print Statement for the last time

--- SaveTheFrenchies/ViewController.swift
+++ SaveTheFrenchies/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Hello Again, Theodore")
+        print("Theodore")
     }
 
     override func didReceiveMemoryWarning() {
