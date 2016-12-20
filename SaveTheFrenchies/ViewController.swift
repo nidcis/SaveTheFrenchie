e3383ad531de8eacefeb46ec94bd570f54365919
David Sajous <david@nidcis.com>
2016-12-20 17:54:40 -0500

    Changed the Print Statement again

--- SaveTheFrenchies/ViewController.swift
+++ SaveTheFrenchies/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Nice Try")
+        print("Hello Again, Theodore")
     }
 
     override func didReceiveMemoryWarning() {
